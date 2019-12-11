26b7cefd409c897c8f9a6f4b05d577442aa3500b
Rolando Lopantzi <rlopantzi@gmail.com>
2019-12-10 17:25:25 -0800

    init commit

---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/rolandolopantzi/Documents/CodingDojo/Week3_5/python_stack/django/django_intro/semi_restful_tv/env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
